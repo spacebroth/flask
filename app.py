320d77fce69f2f5ed0c0121c885ea3a94a50c6ab
spacebroth <spacebroth@gmail.com>
2022-11-12 20:17:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,30 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index/')
+@app.route('/index')
 def index():
-    return 'Hello WORLD'
+    return 'index'
 
 
+@app.route('/contact/')
+def contact():
+    return 'contact information'
 
 
+@app.route('/calculate/7/9/')
+def calculate():
+    return str(7 ** 9)
 
 
+@app.route('/albums/1')
+def hello_world():
+    return 'Hello, Flask'
 
 
-
-
-
-
-
-
+@app.route('/albums/<album_number>')
+def albums(album_number):
+    return 'The {} album.'.format(album_number)
 
 # if __name__ == "__main__":
 #     app.run(host='127..0.0.1', port=5000)
