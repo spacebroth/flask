8f7f4a9322aff22ccbdb9eb377ca0269490e6d82
spacebroth <spacebroth@gmail.com>
2022-11-12 19:29:51 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello WORLD'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     app.run(host='127..0.0.1', port=5000)
